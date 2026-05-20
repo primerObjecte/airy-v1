92d21c190f0a38eb6731765528964f23385f58ff
primerObjecte <primerzero6@gmail.com>
2026-05-20 14:55:43 +0300

    Add CommandContext dataclass for command handling

--- core/context.py
+++ core/context.py
@@ -1,1 +1,11 @@
+from dataclasses import dataclass
+from typing import Any
 
+
+@dataclass
+class CommandContext:
+    runtime: Any
+    event: Any
+    storage: Any
+    reply: Any
+    sender_id: int | None = None
