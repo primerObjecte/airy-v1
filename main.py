cf7ecfd493620e20593d12e42c3a953f97b9b755
primerObjecte <primerzero6@gmail.com>
2026-05-20 14:54:09 +0300

    Add asynchronous runtime execution to main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import asyncio
+from core.runtime import AiryRuntime
 
+if __name__ == "__main__":
+    runtime = AiryRuntime()
+    try:
+        asyncio.run(runtime.start())
+    except KeyboardInterrupt:
+        try:
+            asyncio.run(runtime.stop())
+        except RuntimeError:
+            pass
