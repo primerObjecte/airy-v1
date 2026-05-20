ea69b3a60b5ac1511fd7c907f70aa64b5f6ad190
primerObjecte <primerzero6@gmail.com>
2026-05-20 16:30:40 +0300

    Fix: Добавлена загрузка .env файла в main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import asyncio
+import os
+from dotenv import load_dotenv
 from core.runtime import AiryRuntime
 
 if __name__ == "__main__":
+    load_dotenv()
     runtime = AiryRuntime()
     try:
         asyncio.run(runtime.start())
